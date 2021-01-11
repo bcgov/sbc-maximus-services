fbd42f111ee620cc299df5690c4bcf1640e26dde
Gary Jipp <37557999+gary-jipp@users.noreply.github.com>
2021-01-10 19:37:51 -0800

    Add /api/status endpoint

--- mcap-schedule/src/index.js
+++ mcap-schedule/src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
-require("dotenv").config();
+const axios = require("axios");
+require("dotenv").config({ path: `${__dirname}/.env` });
 /*=============================================================================
 Schedule Service
 =============================================================================*/
@@ -10,7 +11,11 @@ const SERVICE_PORT = process.env.SERVICE_PORT || 8080;
 
 let envConfig = {
   SCHEDULE_URL: process.env.SCHEDULE_URL,
+  CHAT_STATUS_URL: process.env.CHAT_STATUS_URL,
 };
+
+const sslRootCAs = require('ssl-root-cas');
+sslRootCAs.inject();
 
 // console.log(__dirname);
 console.log(envConfig);
@@ -22,8 +27,32 @@ app.get("/hello", function (req, res) {
 
 // send test/prod config to client
 app.get("/api/test", function (req, res) {
-  res.type("json");
-  res.end(JSON.stringify(envConfig));
+  res.json(envConfig);
+});
+
+// send test/prod config to client
+app.get("/api/status/:service", function (req, res) {
+  const service = req.params.service;
+  const url = envConfig[`${service.toUpperCase()}_STATUS_URL`];
+  if (!url) {
+    res.status(400);
+    res.json({ error: "bad service" });
+    return;
+  }
+  console.log(url);
+
+  console.log("axios.get");
+  axios.get(url)
+    .then(r => {
+      res.json({ test: r.data });
+    })
+    .catch(err => {
+      console.log(err.message);
+      res.status(500);
+      // res.json({ error: err.errno });
+      res.json({ error: err });
+    });
+
 });
 
 app.listen(SERVICE_PORT);
